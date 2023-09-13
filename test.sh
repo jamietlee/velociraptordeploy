45884b7fb51b63235bdcb637e8ae2361f09f1593
Jamie Lee <jamie.tlee@outlook.com>
2023-09-13 19:45:09 +0930

    new test

--- test.sh
+++ test.sh
@@ -1,17 +1,19 @@
 #!/bin/bash -xe
 sudo mkdir -p /velociraptor
-sudo touch /velociraptor/test.txt
-echo '[Unit]' >> /velociraptor/test.txt
-echo 'Description=Velociraptor linux amd64' >> /velociraptor/test.txt
-echo 'After=syslog.target network.target' >> /velociraptor/test.txt
-echo '\n' >> /velociraptor/test.txt
-echo '[Service]' >> /velociraptor/test.txt
-echo 'Type=simple' >> /velociraptor/test.txt
-echo 'Restart=always' >> /velociraptor/test.txt
-echo 'RestartSec=120' >> /velociraptor/test.txt
-echo 'LimitNOFILE=20000' >> /velociraptor/test.txt
-echo 'Environment=LANG=en_US.UTF-8' >> /velociraptor/test.txt
-echo 'ExecStart=/opt/velociraptor/velociraptor-v0.6.3-linux-amd64 --config /opt/velociraptor/server.config.yaml frontend -v' >> /velociraptor/test.txt
-echo '\n' >> /velociraptor/test.txt
-echo '[Install]' >> /velociraptor/test.txt
-echo 'WantedBy=multi-user.target' >> /velociraptor/test.txt
+
+sudo cat > /velociraptor/test.txt << EOF
+[Unit]
+Description=Velociraptor linux amd64
+After=syslog.target network.target
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=120
+LimitNOFILE=20000
+Environment=LANG=en_US.UTF-8
+ExecStart=/opt/velociraptor/velociraptor-v0.6.3-linux-amd64 --config /opt/velociraptor/server.config.yaml frontend -v' >> /velociraptor/test.txt
+
+[Install]
+WantedBy=multi-user.target
+EOF
