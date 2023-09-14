43b889298f0a3997c39b0a1675f351751f29709d
Jamie Lee <jamie.tlee@outlook.com>
2023-09-14 17:27:16 +0930

    updated deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,28 @@
 #!/bin/bash -xe
 sudo mkdir /opt/velociraptor
+sudo cp server.config.yaml /opt/velociraptor
 sudo wget https://github.com/Velocidex/velociraptor/releases/download/v0.7.0/velociraptor-v0.7.0-2-linux-amd64 -P /opt/velociraptor/
 sudo chmod +x /opt/velociraptor/velociraptor-v0.7.0-2-linux-amd64
 
 sudo touch /lib/systemd/system/velociraptor.service
-sudo bash -c 'echo [Unit] >> /lib/systemd/system/velociraptor.service'
-sudo bash -c 'echo Description=Velociraptor linux amd64 >> /lib/systemd/system/velociraptor.service'
-sudo bash -c 'echo After=syslog.target network.target >> /lib/systemd/system/velociraptor.service'
-sudo bash -c 'echo \n >> /lib/systemd/system/velociraptor.service'
-sudo bash -c 'echo [Service] >> /lib/systemd/system/velociraptor.service'
-sudo bash -c 'echo Type=simple >> /lib/systemd/system/velociraptor.service'
-sudo bash -c 'echo Restart=always >> /lib/systemd/system/velociraptor.service'
-sudo bash -c 'echo RestartSec=120 >> /lib/systemd/system/velociraptor.service'
-sudo bash -c 'echo LimitNOFILE=20000 >> /lib/systemd/system/velociraptor.service'
-sudo bash -c 'echo Environment=LANG=en_US.UTF-8 >> /lib/systemd/system/velociraptor.service'
-sudo bash -c 'echo ExecStart=/opt/velociraptor/velociraptor-v0.6.3-linux-amd64 --config /opt/velociraptor/server.config.yaml frontend -v >> /lib/systemd/system/velociraptor.service'
-sudo bash -c 'echo \n >> /lib/systemd/system/velociraptor.service'
-sudo bash -c 'echo [Install] >> /lib/systemd/system/velociraptor.service'
-sudo bash -c 'echo WantedBy=multi-user.target >> /lib/systemd/system/velociraptor.service'
+#chmod u+w /lib/systemd/system/velociraptor.service
 
-sudo systemctl daemon-reload
-sudo systemctl enable --now velociraptor
+sudo cat > velociraptor.service.txt << EOF
+[Unit]
+Description=Velociraptor linux amd64
+After=syslog.target network.target
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=120
+LimitNOFILE=20000
+Environment=LANG=en_US.UTF-8
+ExecStart=/opt/velociraptor/velociraptor-v0.7.0-2-linux-amd64 --config /opt/velociraptor/server.config.yaml frontend -v' >> /velociraptor/test.txt
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+#sudo systemctl daemon-reload
+#sudo systemctl enable --now velociraptor
